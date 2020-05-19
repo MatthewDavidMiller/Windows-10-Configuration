566fa205ef4f1547a2fe26f6f4082cb96a92b3d8
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-05-19 19:33:49 -0400

    Split windows 10 configuration to its own repository.

--- windows_scripts/windows_scripts.ps1
+++ windows_scripts/windows_scripts.ps1
@@ -1162,8 +1162,7 @@ function ConfigureNTP {
 
 function ConfigureComputerName {
     $ComputerName = Read-Host 'Enter computer hostname '
-    $DomainCredential = Read-Host -AsSecureString "Enter domain credential."
-    Rename-Computer -NewName "$ComputerName" -DomainCredential $DomainCredential
+    Rename-Computer -NewName "$ComputerName"
 }
 
 function ConfigurePowerOptions {
@@ -1205,77 +1204,4 @@ function MapDrives {
     New-PSDrive -Name "N" -PSProvider "FileSystem" -Root "$Share1" -Persist -Scope "Global"
     New-PSDrive -Name "O" -PSProvider "FileSystem" -Root "$Share2" -Persist -Scope "Global"
     New-PSDrive -Name "P" -PSProvider "FileSystem" -Root "$Share3" -Persist -Scope "Global"
-}
-
-function GetFileHashes {
-    # Prompts
-    $MD5 = Read-Host 'Get MD5 checksums? y/n '
-    $SHA1 = Read-Host 'Get SHA1 checksums? y/n '
-    $SHA256 = Read-Host 'Get SHA256 checksums? y/n '
-    $SHA512 = Read-Host 'Get SHA512 checksums? y/n '
-    $Source = Read-Host 'Specify source directory to get file hashes '
-
-    # Get MD5 checksums
-    if ($MD5 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm MD5 | Format-List
-    }
-
-    # Get SHA1 checksums
-    if ($SHA1 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA1 | Format-List
-    }
-
-    # Get SHA256 checksums
-    if ($SHA256 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA256 | Format-List
-    }
-
-    # Get SHA512 checksums
-    if ($SHA512 -eq 'y') {
-        Get-FileHash "$Source\*" -Algorithm SHA512 | Format-List
-    }
-
-    Read-Host -Prompt "Files Hashed.  Press enter to continue."
-}
-
-function ConfigureTime {
-    $Date = Read-Host "Enter date in format xx/xx/xxxx"
-    $Time = Read-Host "Enter time in format xx:xxpm"
-    $Timezone = Read-Host "Enter timezone. A formatting example is: US Eastern Standard Time"
-    Set-Date "($Date) ($Time)"
-    Set-TimeZone "$Timezone"
-}
-
-function ConfigureNetwork {
-    $IPAddress = Read-Host "Enter IP address"
-    $PrefixLength = Read-Host "Enter prefix length"
-    $DefaultGateway = Read-Host "Enter default gateway"
-    $InterfaceAlias = Read-Host "Enter interface alias"
-    $DNSServerAddress = Read-Host "Enter DNS servers"
-    New-NetIPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway -InterfaceAlias $InterfaceAlias
-    Set-DnsClientServerAddress-InterfaceAlias $InterfaceAlias -ServerAddresses $DNSServerAddress
-}
-
-function BlockICMPRequests {
-    Write-Host "Proceeding to block ICMP requests."
-    netsh advfirewall firewall add rule name="ICMP Block incoming V4 echo request" protocol=icmpv4:8, any dir=in action=block
-}
-
-function AddLocalUser {
-    $LocalUserUsername = Read-Host -AsSecureString "Enter username for new user."
-    $LocalUserPassword = Read-Host -AsSecureString "Enter password for new user."
-    $LocalUserFullName = Read-Host -AsSecureString "Enter full name for new user."
-    $LocalUserDescription = Read-Host -AsSecureString "Enter description for new user."
-    New-LocalUser "$LocalUserUsername" -Password $LocalUserPassword -FullName "$LocalUserFullName" -Description "$LocalUserDescription"
-}
-
-function ConfigureSSH {
-    Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH.Server*' | Add-WindowsCapability -Online
-    Set-Service -Name sshd -StartupType 'Automatic'
-    Start-Service sshd
-    Install-Module -Force OpenSSHUtils -Scope AllUsers
-    Set-Service -Name ssh-agent -StartupType 'Automatic'
-    Start-Service ssh-agent
-    Set-Location ~\.ssh\
-    ssh-keygen
 }
