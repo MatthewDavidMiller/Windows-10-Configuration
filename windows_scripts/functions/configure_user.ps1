e95a5597efa1301ec98fda2f71f402c1194c26bf
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-12-24 23:05:36 -0500

    Fixed some issues in users configuration.

--- windows_scripts/functions/configure_user.ps1
+++ windows_scripts/functions/configure_user.ps1
@@ -3,15 +3,15 @@
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.localaccounts/add-localgroupmember?view=powershell-5.1
 
 function CreateUser {
-    $UserName = Read-Host 'Specify username ' -AsSecureString
+    $UserName = Read-Host 'Specify username '
     $Password = Read-Host 'Specify password ' -AsSecureString
-    $FullName = Read-Host 'Specify Full Name '-AsSecureString
-    $Description = Read-Host 'Specify Description of user ' -AsSecureString
-    New-LocalUser "$UserName" -Password "$Password" -FullName "$FullName" -Description "$Description"
+    $FullName = Read-Host 'Specify Full Name '
+    $Description = Read-Host 'Specify Description of user '
+    New-LocalUser "$UserName" -Password $Password -FullName "$FullName" -Description "$Description"
 }
 
 function AddUserToLocalAdmin {
     $AdminGroup = 'Administrators'
-    $User = Read-Host 'Specify user to add to local admin group ' -AsSecureString
+    $User = Read-Host 'Specify user to add to local admin group '
     Add-LocalGroupMember -Group "$AdminGroup" -Member "$User"
 }
